758941fa64aa84deb89faddb8383a0cd76010bbc
Dasom Lim <ektha03@gmail.com>
2018-05-18 05:39:27 +0900

    3.4.2 코드를 이용하는 수동 DI (setDataSource에서)

--- src/main/java/com/dasom/ex/userDao/UserDao.java
+++ src/main/java/com/dasom/ex/userDao/UserDao.java
@@ -16,13 +16,10 @@ public class UserDao {
 	public UserDao() {};
 	
 	public void setDataSource(DataSource dataSource) {
+		this.jdbcContext = new JdbcContext();
+		this.jdbcContext.setDataSource(dataSource);
 		this.dataSource = dataSource;
 	}
-	
-	public void setJdbcContext(JdbcContext jdbcContext) {
-		this.jdbcContext = jdbcContext;
-	}
-
 	
 	public void deleteAll() throws SQLException{
 		this.jdbcContext.workWirhStatementStrategy(new StatementStrategy() {
